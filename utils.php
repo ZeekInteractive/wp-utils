40951d3307f6985eb08cbc184db3a12d8cf2f153
Philip Newcomer <philip@zeek.com>
2017-05-03 09:56:21 -0400

    Add get_current_url() function

--- utils.php
+++ utils.php
@@ -1,3 +1,14 @@
 <?php
 
 namespace Zeek\WP_Util;
+
+/**
+ * Returns the current URL.
+ *
+ * @link http://wordpress.stackexchange.com/a/126534
+ *
+ * @return string
+ */
+function get_current_url() {
+	return home_url( add_query_arg( null, null ) );
+}
