7f7d986e4df6fd571010742a94884a58d1566674
Philip Newcomer <philip@zeek.com>
2017-05-24 10:20:38 -0400

    Add get_current_url_clean() function

--- utils.php
+++ utils.php
@@ -12,3 +12,16 @@ namespace Zeek\WP_Util;
 function get_current_url() {
 	return home_url( add_query_arg( null, null ) );
 }
+
+/**
+ * Returns the current URL, but without query args.
+ *
+ * @return string
+ */
+function get_current_url_clean() {
+	$current_url = get_current_url();
+
+	$url_parts = parse_url( $current_url );
+
+	return home_url( $url_parts['path'] );
+}
