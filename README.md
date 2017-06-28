0888e12b6653bc850a8b89ebffac7c4e7b7c0141
Aaron Holbrook <aaron@zeek.com>
2017-06-28 16:19:33 -0500

    Add get_id_from_slug function

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ A collection of functions that provide utility functionality for WordPress.
 
 * `get_current_url()`: Returns the current URL.
 * `get_current_url_clean()`: Returns the current URL, but without query args.
+* `get_id_from_slug( $slug, $post_type = 'post', $force = false )` : Performs a lookup for a post given a slug.
--- utils.php
+++ utils.php
@@ -25,3 +25,45 @@ function get_current_url_clean() {
 
 	return home_url( $url_parts['path'] );
 }
+
+/**
+ * Performs a reverse lookup of a post based on it's slug
+ * Stores in whatever default cache is available in order to minimize duplicate
+ * lookups (as this can get expensive)
+ *
+ * @return int|null
+ */
+function get_id_from_slug( $slug, $post_type = 'post', $force = false ) {
+
+	global $wpdb;
+
+	$cache_key = sprintf( 'post_%s_id', md5( $post_type . $slug ) );
+	$id        = wp_cache_get( $cache_key );
+
+	if ( false === $id || $force ) {
+
+		$sql = sprintf( "
+			SELECT 
+				ID
+			FROM 
+				%s
+			WHERE 
+				post_name = '%s' AND
+				post_type = '%s'
+			",
+			$wpdb->posts,
+			sanitize_text_field( $slug ),
+			sanitize_text_field( $post_type )
+		);
+
+		$id = $wpdb->get_var( $sql );
+
+		wp_cache_set( $cache_key, $id );
+	}
+
+	if ( empty( $id ) ) {
+		return null;
+	}
+
+	return intval( $id );
+}
