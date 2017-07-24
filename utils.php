14a4b4278af8cb578921a782b24245efcb1028b4
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2017-07-24 16:18:39 -0500

    Update utils.php
    Add `remove_filters_for_anonymous_class` function

--- utils.php
+++ utils.php
@@ -97,30 +97,41 @@ function get_raw_option_value( $key ) {
 
 	return intval( $version );
 }
-
 /**
- * Helper function to check for an environmental variable in a variety of places:
- * - $_ENV (for setting via .env.php files)
- * - Constant (for setting via a define() call)
- * - Filter, utilizing a passed in filter
+ * Allow to remove method for an hook when, it's a class method used and
+ * class don't have variable, but you know the class name
  * 
- * @param      $key
- * @param null $filter
+ * @param string $hook_name
+ * @param string $class_name
+ * @param string $method_name
+ * @param int    $priority
  *
- * @return mixed|null
+ * @return bool
  */
-function get_env_value( $key, $filter = null ) {
-	if ( ! empty( $_ENV[ $key ] ) ) {
-		return $_ENV[ $key ];
+function remove_filters_for_anonymous_class( $hook_name = '', $class_name = '', $method_name = '', $priority = 0 ) {
+	global $wp_filter;
+
+	// Take only filters on right hook name and priority
+	if ( ! isset( $wp_filter[ $hook_name ][ $priority ] ) || ! is_array( $wp_filter[ $hook_name ][ $priority ] ) ) {
+		return false;
 	}
 
-	if ( defined( $key ) ) {
-		return constant( $key );
-	}
-	
-	if ( function_exists( $filter ) ) {
-		return apply_filters( $filter, null ); 
+	// Loop on filters registered
+	foreach ( (array) $wp_filter[ $hook_name ][ $priority ] as $unique_id => $filter_array ) {
+		// Test if filter is an array ! (always for class/method)
+		if ( isset( $filter_array['function'] ) && is_array( $filter_array['function'] ) ) {
+			// Test if object is a class, class and method is equal to param !
+			if ( is_object( $filter_array['function'][0] ) && get_class( $filter_array['function'][0] ) && get_class( $filter_array['function'][0] ) == $class_name && $filter_array['function'][1] == $method_name ) {
+				// Test for WordPress >= 4.7 WP_Hook class (https://make.wordpress.org/core/2016/09/08/wp_hook-next-generation-actions-and-filters/)
+				if ( is_a( $wp_filter[ $hook_name ], 'WP_Hook' ) ) {
+					unset( $wp_filter[ $hook_name ]->callbacks[ $priority ][ $unique_id ] );
+				} else {
+					unset( $wp_filter[ $hook_name ][ $priority ][ $unique_id ] );
+				}
+			}
+		}
+
 	}
 
-	return null;
+	return false;
 }
