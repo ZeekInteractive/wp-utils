50346692d7413dfa68c7bde2ff88d1839d5f4ec9
Aaron Holbrook <aaron@zeek.com>
2018-06-13 13:42:13 -0500

    Add sentry support

---
+++ src/Sentry.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Zeek\WP_Util;
+
+class ErrorHandling {
+	private static $client;
+
+	static function init() {
+		$sentry_url = get_env_value( 'SENTRY_URL' );
+
+		if ( empty( $sentry_url ) ) {
+			return false;
+		}
+
+		if ( ! empty( self::$client ) ) {
+			return self::$client;
+		}
+
+		self::$client = new \Raven_Client( get_env_value( 'SENTRY_URL' ) );
+		self::$client->install();
+
+		return self::$client;
+	}
+
+	static function get_client() {
+		if ( empty( self::$client ) ) {
+			self::init();
+		}
+
+		return self::$client;
+	}
+}
