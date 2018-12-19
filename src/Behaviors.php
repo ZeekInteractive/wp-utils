1359c1e9a831a37167f5aa6d70318ee31ffc46a5
Aaron Holbrook <aaron@zeek.com>
2018-12-19 05:56:30 -0600

    Add additional behaviors

--- src/Behaviors.php
+++ src/Behaviors.php
@@ -15,6 +15,19 @@ class Behaviors {
 		 * This can be overridden by setting an environmental variable of 'FILE_MOD_ALLOWED'
 		 */
 		add_filter( 'file_mod_allowed', [ $this, 'file_mod_allowed' ], 999 );
+
+
+		/**
+		 * Disables the 'Update WordPress' admin nag notice
+		 */
+		add_filter( 'admin_init', [ $this, 'disable_update_nag'] );
+
+		/**
+		 * Removes the 'Thank you for creating with WordPress' message 
+		 * Removes the 'Version x.x.x | Get Version x.x.x'
+		 * From the bottom of all admin pages
+		 */
+		add_filter( 'admin_init', [ $this, 'remove_admin_footer_text_version'] );
 	}
 
 	function file_mod_allowed() {
@@ -23,5 +36,15 @@ class Behaviors {
 		}
 
 		return false;
+	}
+
+	function disable_update_nag() {
+		remove_action( 'admin_notices', 'update_nag', 3 );
+		remove_action( 'network_admin_notices', 'update_nag', 3 );
+	}
+
+	function remove_admin_footer_text_version() {
+		add_filter( 'admin_footer_text', '__return_empty_string', 200 );
+		add_filter( 'update_footer', '__return_empty_string', 200 );
 	}
 }
