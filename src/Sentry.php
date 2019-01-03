e12986c2ba600f076a85335d5bdd89235e2d5490
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2019-01-03 06:47:31 -0600

    Add exclusions to abort sentry sending

--- src/Sentry.php
+++ src/Sentry.php
@@ -5,6 +5,11 @@ namespace Zeek\WP_Util;
 class ErrorHandling {
 	private static $client;
 
+	public static $exclusions = [
+		'Parameter 1 to wp_default_scripts() expected to be a reference, value given',
+		'Parameter 1 to wp_default_styles() expected to be a reference, value given',
+	];
+
 	static function init() {
 		$sentry_url = get_env_value( 'SENTRY_URL' );
 
@@ -16,28 +21,40 @@ class ErrorHandling {
 			return self::$client;
 		}
 
-		self::$client = new \Raven_Client( $sentry_url );
+		$exclusions = self::$exclusions;
+
+		$options = [
+			'send_callback' => function ( $data ) use ( $exclusions ) {
+
+				if ( 'error' === $data['level'] ) {
+					return $data;
+				}
+
+				$value = $data['exception']['values'][0]['value'] ?? null;
+
+				if ( empty( $value ) ) {
+					return $data;
+				}
+
+				if ( in_array( $value, $exclusions ) ) {
+					return false;
+				}
+
+				return $data;
+			},
+		];
+
+		self::$client = new \Raven_Client( $sentry_url, $options );
 		self::$client->install();
 
 		return self::$client;
 	}
 
-	/**
-	 * @return \Raven_Client
-	 */
 	static function get_client() {
 		if ( empty( self::$client ) ) {
 			self::init();
 		}
 
 		return self::$client;
-	}
-	
-	static function capture( $exception ) {
-		if ( empty( self::$client ) ) {
-			return;
-		}
-
-		self::$client->captureException( $exception );
 	}
 }
