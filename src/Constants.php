d0ef3fb2e6899e2f6edb71cf13793d25240e00fc
Aaron Holbrook <AaronHolbrook@users.noreply.github.com>
2019-03-27 13:31:33 -0500

    Create Constants.php

---
+++ src/Constants.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Zeek\WP_Util;
+
+use Arrilot\DotEnv\DotEnv;
+
+class Constants {
+
+	private static $env_vars = [
+		'ACF_LITE'         => true,
+		'DISABLE_WP_CRON'  => false,
+		'FILE_MOD_ALLOWED' => false,
+		'SENTRY_URL'       => null,
+	];
+
+	public static function init( $dir ) {
+		/**
+		 * Load dotenv if .env file is present
+		 */
+		if ( file_exists( $dir . '/.env.php' ) ) {
+			DotEnv::load( $dir . '/.env.php' );
+			DotEnv::copyVarsToEnv();
+		}
+
+		// Set usable constants
+		define( 'APP_URL', plugin_dir_url( $dir ) );
+		define( 'APP_PATH', dirname( $dir ) . '/' );
+
+		/**
+		 * Set the proper constant for ACF Lite
+		 * Only load our hardcoded fields if ACF Lite is off
+		 */
+		foreach ( self::$env_vars as $key => $default ) {
+			self::env_set( $key, $default );
+		}
+	}
+
+	private static function env_set( $key, $default ) {
+		// Check if we have env value
+		$env_value = env( $key, $default );
+
+		if ( ! isset( $env_value ) ) {
+			return false;
+		}
+
+		// if not defined
+		if ( defined( $key ) ) {
+			return false;
+		}
+
+		define( $key, $env_value );
+	}
+}
