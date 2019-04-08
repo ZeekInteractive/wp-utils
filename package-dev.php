fb9c010ac4d71a29da69dd9d1b1f6d6651ad787a
Aaron Holbrook <aaron@zeek.com>
2019-04-08 06:57:54 -0500

    Add autoload-dev file

---
+++ package-dev.php
@@ -0,0 +1,7 @@
+<?php
+
+if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
+  require_once __DIR__ . '/vendor/autoload.php';
+}
+
+\A7\autoload( __DIR__ . '/src' );
--- package.php
+++ package.php
@@ -1,7 +1,3 @@
 <?php
 
-if ( file_exists( __DIR__ . '/vendor/autoload.php' ) ) {
-  require_once __DIR__ . '/vendor/autoload.php';
-}
-
 \A7\autoload( __DIR__ . '/src' );
