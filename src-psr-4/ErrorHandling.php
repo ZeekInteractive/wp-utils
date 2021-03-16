457e777557e54c17258be564c401ee4bbfd8f1b2
Aaron Holbrook <aaron@zeek.com>
2021-03-16 06:36:14 -0500

    Add psr-4 autoloading. Add ErrorHandling class with interface

---
+++ src-psr-4/ErrorHandling.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Zeek\Wp_Util;
+
+use Zeek\WP_Util\Interfaces\ErrorHandling as ErrorHandlingInterface;
+
+class ErrorHandling implements ErrorHandlingInterface {
+	public static function capture( \Exception $exception ) : void {
+		$test = 1;
+	}
+}
