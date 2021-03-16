1065c820ca20aa8f9a4aa93786a64fad2a64f4d2
Aaron Holbrook <aaron@zeek.com>
2021-03-16 06:56:30 -0500

    Add error handling interface, update wp-sentry to 1.1

--- src-psr-4/ErrorHandling.php
+++ src-psr-4/ErrorHandling.php
@@ -3,9 +3,12 @@
 namespace Zeek\WP_Util;
 
 use Zeek\WP_Util\Interfaces\ErrorHandling as ErrorHandlingInterface;
+use Zeek\WpSentry\WpSentry;
 
 class ErrorHandling implements ErrorHandlingInterface {
 	public static function capture( \Exception $exception ) : void {
-		$test = 1;
+		$sentry = WpSentry::getInstance();
+
+		$sentry->captureException($exception);
 	}
 }
